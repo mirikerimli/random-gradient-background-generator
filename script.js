b735c3a5dddf08b5128999f4451bcf2d5a3a7be7
Mirkarim <148642209+mirikerimli@users.noreply.github.com>
2023-11-11 02:45:27 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+var btn = document.querySelector("button");
+
+
+function setGradient() {
+	body.style.background = 
+	"linear-gradient(to right, " 
+	+ color1.value 
+	+ ", " 
+	+ color2.value 
+	+ ")";
+
+	css.textContent = body.style.background + ";";
+}
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
+
+btn.addEventListener("click", function() {
+	var randomColor1 = Math.floor(Math.random()*16777215).toString(16);
+	var randomColor2 = Math.floor(Math.random()*16777215).toString(16);
+	color1.value = "#" + randomColor1;
+	color2.value = "#" + randomColor2;
+	setGradient();
+})
+
+setGradient();
